0f28bf1891483fe5597be424701d0db27e2aa1d2
jbiscan-dot <jbiscan21@gmail.com>
2024-05-01 20:01:29 -0400

    aidans goated coding

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,27 +1,25 @@
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <esp32-hal-ledc.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-#define BRAKE 33
-
-#define PWM1 4
+#define CHANNEL 0
+#define BRAKE 27
+#define PWM1 14
 #define PWM1_CH 1
-#define DIR1 2
-
+#define DIR1 12
 #define TIMER_BIT  8
 #define MOTOR_FREQUENCY  20000
-
 #define POT_KP 27
 #define POT_KI 26
 #define POT_KD 32
-#define AIN1 18 // Define these pins if not already defined
+#define AIN1 18
 #define AIN2 19
 
-// IMU functions;
 sensors_event_t a, g, temp;
 Adafruit_MPU6050 mpu;
 
-// Define physical constants
 const float pi = 3.14159265359;
 
 enum motorMode { shortBrake, CCW, CW, stop };
@@ -34,10 +32,8 @@ struct calibrate {
   sensors_vec_t gyro;
   sensors_vec_t acceleration;
 };
-
 struct calibrate Calibration;
 
-// Alpha-beta filter values
 float roll = 0;
 float theta_roll;
 float pitch = 0;
@@ -46,14 +42,14 @@ float roll_dot = 0;
 float pitch_dot = 0;
 float alpha = 0.8;
 float beta = 0.1;
-const int T_int = 50; // Integration period in ms
+const int T_int = 50;
 
-// PID Parameters
-double KP = 0; // Gains - tune later
-double KI = 0;
-double KD = 0;
-float setpoint = 2.49; // 0 degrees?
-float SETPOINT = 2.49;
+float KP;
+float KI;
+float KD;
+
+float setpoint = -0.95;
+float SETPOINT = -0.95;
 float integral = 0;
 float error = 0;
 float previousError = 0;
@@ -63,33 +59,66 @@ int time_ = 0;
 int prevTime = 0;
 int time_delta = 0;
 
-const int ControlLoopFrequency = 75; // Increase for faster response
-
-
 unsigned long balancingStartTime;
 
 void pwmSet(uint8_t channel, uint32_t value) {
   ledcWrite(channel, value);
 }
 
+int receivedKP;
+int receivedKI;
+int receivedKD ;
+
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int len) {
+  // if (len == sizeof(float)) {
+    // receivedKP = *(float *)data;
+      // Allocate memory for the identifier
+    char identifier[4]; // Assuming the maximum length of the identifier is 3 characters
+
+    // Copy the identifier from the received data
+    strncpy(identifier, (char*)data, 3);
+    identifier[3] = '\0'; // Ensure null termination
+
+    if (strncmp(identifier, "KP=", 3) == 0){
+      sscanf((char*)data,"KP=%f", &KP);
+      Serial.print("Received KP: ");
+      Serial.println(KP);
+    }else if (strncmp(identifier, "KI=", 3) == 0){
+      sscanf((char*)data,"KI=%f", &KI);
+      Serial.print("Received KI: ");
+      Serial.println(KI);
+    }else if (strncmp(identifier, "KD=", 3) == 0){
+      sscanf((char*)data,"KD=%f", &KD);
+      Serial.print("Received KD: ");
+      Serial.println(KD);
+    }
+
+    // Serial.println(receivedKP);
+    // delay(3000);
+  //   KP = receivedKP;
+  //   Serial.println("Received KP: ");
+  //   //Serial.println(receivedKP);
+  // } else if (len == 2 * sizeof(float)) {
+  //   receivedKI = *(float *)(data + sizeof(float));
+  //   KI = receivedKI;
+  //   Serial.println("Received KI: ");
+  //   //Serial.println(receivedKI);
+  // } else if (len == 3 * sizeof(float)) {
+  //   receivedKD = *(float *)(data + 2 * sizeof(float));
+  //   KD = receivedKD;
+  //   Serial.println("Received KD: ");
+  //   //Serial.println(receivedKD);
+  // }
+}
+
 void setup() {
-  // Configure PWM channel
-  ledcSetup(PWM1_CH, MOTOR_FREQUENCY, TIMER_BIT); // PWM channel 0, 8-bit resolution
-
-  // Attach PWM channel to GPIO pin
-  ledcAttachPin(PWM1, PWM1_CH); // Use PWM channel 0
-
   pinMode(BRAKE, OUTPUT);
   digitalWrite(BRAKE, HIGH);
-
-  motorControl(0, 0);
-
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
   pinMode(DIR1, OUTPUT);
-  pinMode(POT_KP, INPUT);
-  pinMode(POT_KI, INPUT);
-  pinMode(POT_KD, INPUT);
+  ledcSetup(PWM1_CH, MOTOR_FREQUENCY, TIMER_BIT);
+  ledcAttachPin(PWM1, PWM1_CH);
+  motorControl(0, 0); 
+
   Serial.begin(115200);
 
   if (!mpu.begin()) {
@@ -102,121 +131,77 @@ void setup() {
 
   calibrateSensors();
 
-  // Set time
+  delay(1000);
+
   time_ = millis();
   prevTime = time_;
 
   balancingStartTime = millis();
+
+  WiFi.mode(WIFI_STA);
+  
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop() {
-
-  if (Serial.available() > 0) {
-      // read the incoming bytes
-      char incomingByte = Serial.read();
-
-      // update the variables based on the received byte
-      if (incomingByte == 'KP') {
-        KP = readIntFromSerial();
-      } else if (incomingByte == 'KI') {
-        KI = readIntFromSerial();
-      } else if (incomingByte == 'KD') {
-        KD = readIntFromSerial();
-      }
-  }
-
-
-
   prevTime = time_;
   time_ = millis();
-
   time_delta = time_ - prevTime;
-
-  kp_val = analogRead(POT_KP);
-  ki_val = analogRead(POT_KI);
-  kd_val = analogRead(POT_KD);
-
-  KP = map(kp_val, 0, 4095, 3000, 3500);
-  KI = map(ki_val, 0, 4095, 500, 5000);
-  KD = map(kd_val, 0, 4095,500, 800);
-
-  // KP = 3090;
-  // KI = 2500;
-  // KD = 500;
-
-
-  // Serial.print(" KP:");
-  // Serial.print(kp_val);
-  // Serial.print(" ");
-  // Serial.print(KP);
-  // Serial.print(" KI:");
-  //  Serial.print(KI);
-  // Serial.print(" KD:");
-  // Serial.println(KD);
 
   getAngles();
 
-    // Add dithering to prevent acceleration in one direction at equilibrium
-  if(pitch>=SETPOINT){
-    setpoint -= .05*time_delta;
-  }else{
-    setpoint += .05*time_delta;
+  if(pitch >= SETPOINT){
+    setpoint -= 0.05 * time_delta;
+  } else {
+    setpoint += 0.05 * time_delta;
   }
-  setpoint = constrain(setpoint, SETPOINT - .01, SETPOINT + .01);
+  setpoint = constrain(setpoint, SETPOINT - 0.01, SETPOINT + 0.01);
 
-  error = setpoint - theta_pitch;
-
-  // Serial.print(" setpoint:");
-  // // Serial.print(setpoint);
-  // Serial.print(" Pitch:");
-  // Serial.print(pitch);
+  error = setpoint - theta_roll;
 
   previousError = error;
-  derivative = g.gyro.x;
-  integral += error * (time_ - prevTime)/1000;
+  derivative = g.gyro.y;
+  integral += error * (time_ - prevTime) / 1000;
   integral = constrain(integral, -255, 255);
 
   PIDout = KP * error + KI * integral - KD * derivative;
   speed = constrain(PIDout, -255, 255);
 
-  // Serial.print(" Speed:");
-  // Serial.println(speed);
+  Serial.print("KP = ");
+  Serial.print(KP);
+  Serial.print(" KI = ");
+  Serial.print(KI);
+  Serial.print(" KD = ");
+  Serial.print(KD);
+
 
   motorControl(speed, PWM1_CH);
 
-  // unsigned long loopStartTime = millis();
-
-  //   // Delay to control loop frequency
-  // unsigned long loopEndTime = millis();
-  // int loopDuration = loopEndTime - loopStartTime;
-  // if (loopDuration < 1000 / ControlLoopFrequency) {
-  //   delay(1000 / ControlLoopFrequency - loopDuration);
-  // }
-  delay(10);
+  delay(20);
+  Serial.println("");
 }
 
 void getAngles() {
-  // get imu data and calculate angles
   mpu.getEvent(&a, &g, &temp);
   getEulerAngles(a.acceleration);
   theta_roll = alphaFilter(roll, theta_roll);
   theta_pitch = alphaFilter(pitch, theta_pitch);
-  // Serial.print("ThetaPitch:");
-  // Serial.print(theta_pitch);
-  // Serial.print(" Pitch:");
-  // Serial.println(pitch);
 }
 
 void setMode(int mode) {
   switch (mode) {
     case shortBrake:
-      digitalWrite(AIN1, HIGH);
-      digitalWrite(AIN2, HIGH);
-      break;
-    case CCW:
-      digitalWrite(DIR1, LOW);
+      Serial.println("");
       break;
     case CW:
+      digitalWrite(DIR1, LOW);
+      break;
+    case CCW:
       digitalWrite(DIR1, HIGH);
       break;
   }
@@ -254,7 +239,7 @@ void calibrateSensors() {
 }
 
 void getEulerAngles(sensors_vec_t a) {
-  pitch = atan2(a.y, a.z); //pitch
+  pitch = atan2(a.y, a.z);
   roll = atan2(a.x, a.z);
 }
 
@@ -285,9 +270,6 @@ void motorControl(int speed, int motorPIN) {
 }
 
 int readIntFromSerial() {
-  // read the incoming bytes until a newline character is received
   String inputString = Serial.readStringUntil('\n');
-  
-  // convert the string to an integer
   return inputString.toInt();
 }
